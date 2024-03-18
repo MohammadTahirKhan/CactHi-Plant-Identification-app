54ee76bb41dafc8f347ec3874453a7c8257cb478
Dan VS Code <dan.stgallay@gmail.com>
2024-03-18 17:29:30 +0000

    Added EJS framework

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,21 @@
+
 var express = require('express');
 var router = express.Router();
+
 var plants = require('../controllers/plants')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   let result = plants.getAll()
   result.then(students => {
     let data = JSON.parse(students);
     console.log(data)
-    res.render('index', { title: 'PLANTS!!!', data: data });
+    res.render('index', {title: 'PLANTS!!!', data: data});
 
   })
 
 });
+
+
 
 module.exports = router;
