874f52ddf2ba8b52b492e377e49c9519217ed8ce
Ethan Watts <ethan.watts@live.co.uk>
2024-03-28 17:51:58 +0000

    add view all plants, plant detail, partials

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var router = express.Router();
 var plants = require('../controllers/plants');
+var plantModel = require('../models/plants');
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -12,7 +14,19 @@ router.get('/', function (req, res, next) {
     res.render('index', {title: 'PLANTS!!!', data: data});
 
   })
+});
 
+router.get('/view-plants', function(req, res, next) {
+  plants.getAll().then(plants => {
+    plants = JSON.parse(plants)
+    res.render('view-all-plants', {title: 'View All Plants', plants: plants});
+  })
+});
+
+router.get('/view-plants/:uid', function(req, res, next) {
+  plantModel.findById(req.params.uid).then(plant => {
+    res.render('plant-detail', {title: `${plant.name} Details`, plant: plant});
+  })
 });
 
 router.post('/changeChat', function (req, res, next) {
@@ -22,9 +36,6 @@ router.post('/changeChat', function (req, res, next) {
   let result = plants.updateField(pushData[0], 'chat',pushData[1]);
   console.log(result);
   res.redirect('/');
-
-
-
 });
 
 
