32dd69c43262961d9d800ab565bf8f9b472eb63c
Mohammad Tahir Khan <tahir026khan@gmail.com>
2024-03-30 15:40:41 +0000

    add plant backend incomplete

--- controllers/plants.js
+++ controllers/plants.js
@@ -1,15 +1,43 @@
 const plantModel = require('../models/plants');
 
-exports.create = (plantData) => {
-    let plant = new plantModel(plantData);
+exports.create = function (plantData, filePath) {
+    // Create a new plant instance using the provided plant data
+    let plant = new plantModel({
+        name: plantData.name,
+        identification_complete: plantData.identification_complete,
+        user: plantData.user,
+        chat: plantData.chat,
+        description: plantData.description,
+        image: filePath, // Assuming this is the field for image path
+        latitude: plantData.latitude,
+        longitude: plantData.longitude,
+        height: plantData.height,
+        spread: plantData.spread,
+        flowers: plantData.flowers,
+        flower_colour: plantData.flower_colour,
+        leaves: plantData.leaves,
+        seeds_or_fruit: plantData.seeds_or_fruit,
+        sun_exposure: plantData.sun_exposure,
+        date_time_added: plantData.date_time_added,
+        last_checked: plantData.last_checked
+    });
 
+    // Save the plant to the database and handle success or failure
     return plant.save().then(plant => {
+        // Log the created plant
+        console.log(plant);
+
+        // Return the plant data as a JSON string
         return JSON.stringify(plant);
     }).catch(err => {
+        // Log the error if saving fails
         console.log(err);
+
+        // Return null in case of an error
         return null;
     });
-}
+};
+
 
 exports.getAll = function () {
     return plantModel.find({}).then(plants => {
--- routes/new-plant-sighting.js
+++ routes/new-plant-sighting.js
@@ -1,16 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var plantsController = require('../controllers/plants');
+var multer = require('multer');
+
+// Multer configuration for file upload
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/images/uploads/');
+    },
+    filename: function (req, file, cb) {
+        var original = file.originalname;
+        var file_extension = original.split(".");
+        // Make the file name the date + the file extension
+        filename =  Date.now() + '.' + file_extension[file_extension.length-1];
+        cb(null, filename);
+    }
+});
+var upload = multer({ storage: storage });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('new-plant-sighting', { title: 'Plant Creation' });
 });
 
-router.post('/',function (req,res){
-    console.log(req.body.username)
-    console.log(req.body["plant_name"])
-    console.log(req.body)
-    res.render('index',{title:"Success"});
+router.post('/', function (req, res) {
+    console.log(req.body.username);
+    console.log(req.body["plant_name"]);
+    console.log(req.body);
+    res.render('index', { title: "Success" });
 });
 
+router.get('/add', function(req, res, next) {
+    res.render('new-plant-sighting', { title: 'Add Plants' });
+});
+
+router.post('/add', upload.single('myImg'), function (req, res, next) {
+    let plantData = req.body;
+    let filePath = req.file.path;
+    let result = plantsController.create(plantData, filePath);
+    console.log(result);
+    res.redirect('/');
+});
 module.exports = router;
