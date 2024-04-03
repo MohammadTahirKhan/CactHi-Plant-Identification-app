41d67b4e2207578381da8ddf025ac61bee9fd9d5
Lucy Slater <lordceecee@gmail.com>
2024-04-03 19:24:09 +0100

    added landing page with some initial styling

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var newPlantRouter = require('./routes/new-plant-sighting');
+var landingPageRouter = require('./routes/landing-page');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/new-plant-sighting', newPlantRouter);
+app.use('/landing-page', landingPageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
