715abe654072f909437c46f93852dd3c1cc1ab2f
Ethan Watts <ethan.watts@live.co.uk>
2024-04-16 18:09:06 +0100

    sorting and tooltips added

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,30 @@ router.get('/', function (req, res, next) {
 router.get('/view-plants', function(req, res, next) {
   plants.getAll().then(plants => {
     plants = JSON.parse(plants)
-    res.render('view-all-plants', {title: 'View All Plants', plants: plants});
+
+    if (req.query.sort === 'recent') {
+      plants.sort((a, b) => b.date_time_added - a.date_time_added);
+
+    } else if (req.query.sort === 'oldest') {
+      plants.sort((a, b) => a.date_time_added - b.date_time_added);
+
+    } else if (req.query.sort === 'unidentified') {
+      plants.sort((a, b) => a.identification_complete - b.identification_complete);
+
+    } else if (req.query.sort === 'closest' || req.query.sort === 'farthest') {
+      location = req.query.location.split(',');
+      plants.sort((a, b) => {
+        aDistance = Math.sqrt(Math.pow(a.latitude - location[0], 2) + Math.pow(a.longitude - location[1], 2));
+        bDistance = Math.sqrt(Math.pow(b.latitude - location[0], 2) + Math.pow(b.longitude - location[1], 2));
+
+        if (req.query.sort === 'closest')
+          return aDistance - bDistance;
+
+        return bDistance - aDistance;
+      });
+    }
+
+    res.render('view-all-plants', {title: 'View All Plants', plants: plants, sort: req.query.sort});
   })
 });
 
